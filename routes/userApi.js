cd1e3cab630d3d75ada4864e7bfd90bd9f67b9a6
Mokhles <mokhleshaj@gmail.com>
2020-09-16 12:15:02 +0200

    crud  with mongoose

--- routes/userApi.js
+++ routes/userApi.js
@@ -1,30 +1,35 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/user')
 //add user
-router.post('/addUser',(req,res,next)=>{
-    console.log(req.body);
-    res.json(req.body)
+router.post('/addUser', async (req,res,next)=>{
+    const user = new User(req.body)
+   await user.save()
+   res.send(user)
 })
 
 // get users 
-router.get('/allUsers', (req,res,next)=>{
-    res.json({message: "get all users"})
+router.get('/allUsers', async (req,res,next)=>{
+    const users = await User.find()
+    res.json(users)
 })
 
 // get user by id
-router.get('/getUser/:id', (req,res,next)=>{
-    res.json({message: "get user num"+req.params.id})
+router.get('/getUser/:id', async (req,res,next)=>{
+    const user = await User.findById(req.params.id).exec()
+    res.json(user)
 })
 
 // delete user 
-router.delete('/delete/:id', (req,res,next)=>{
-    res.json({message: "user number " +req.params.id+  " is deleted"})
+router.delete('/delete/:id', async(req,res,next)=>{
+const user =   await User.findByIdAndDelete(req.params.id).exec()
+res.json(user)  
 })
 
 // update user 
-router.put('/update/:id', (req,res,next)=>{
-    res.json({message: "user number " +req.params.id+  " is updated"})
+router.put('/update/:id', async (req,res,next)=>{
+   await User.findByIdAndUpdate(req.params.id,req.body);
+   res.json(req.body)
 })
 
 module.exports = router
