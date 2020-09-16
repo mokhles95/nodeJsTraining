b434be5bfaab6381504fad725b67e08bbdb0d3b7
Mokhles <mokhleshaj@gmail.com>
2020-09-16 11:04:44 +0200

    projet nodejs Express+ bodyParser

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const http = require('http');
-
+const express = require('express')
+const app = express()
 const hostname = '127.0.0.1';
 const port = 3000;
+var bodyParser = require('body-parser')
+ 
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+app.get('/', (req,res,next)=>{
+    res.send("hello")
+})
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
