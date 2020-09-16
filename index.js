f0b6691ca98fd27add49d006d598ba90d714167f
Mokhles <mokhleshaj@gmail.com>
2020-09-16 11:19:02 +0200

    add static crud user and nodemon

--- index.js
+++ index.js
@@ -27,6 +27,32 @@ app.get('/', (req,res,next)=>{
     res.send("hello")
 })
 
+//add user
+app.post('/users/addUser',(req,res,next)=>{
+    console.log(req.body);
+    res.json(req.body)
+})
+
+// get users 
+app.get('/users/allUsers', (req,res,next)=>{
+    res.json({message: "get all users"})
+})
+
+// get user by id
+app.get('/users/getUser/:id', (req,res,next)=>{
+    res.json({message: "get user num"+req.params.id})
+})
+
+// delete user 
+app.delete('/users/delete/:id', (req,res,next)=>{
+    res.json({message: "user number " +req.params.id+  " is deleted"})
+})
+
+// update user 
+app.put('/users/update/:id', (req,res,next)=>{
+    res.json({message: "user number " +req.params.id+  " is updated"})
+})
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
